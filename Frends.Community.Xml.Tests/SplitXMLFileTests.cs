5165a23ce249977eec3e51dc0be5d33e3ea2f6da
Ville Vuorela <ville.vuorela@hiq.fi>
2020-10-05 16:34:04 +0300

    WIP

---
+++ Frends.Community.Xml.Tests/SplitXMLFileTests.cs
@@ -0,0 +1,102 @@
+using System;
+using NUnit.Framework;
+using Frends.Community.Xml;
+using System.Threading;
+using System.IO;
+using System.Xml;
+using System.Linq;
+
+namespace Frends.Split.Tests
+{
+    [TestFixture]
+    public class SplitXMLFileTests
+    {
+        private string _minifiedInputPath = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestFiles\12_products_minified.xml");
+        private string _prettyInputPath = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestFiles\12_products_pretty.xml");
+        private string _tempOutputFolder;
+
+
+        [SetUp]
+        public void TestSetup()
+        {
+            _tempOutputFolder = Path.Combine(Path.GetTempPath(), "splitxml_tests");
+
+            if (!Directory.Exists(_tempOutputFolder))
+            {
+                Directory.CreateDirectory(_tempOutputFolder);
+            }            
+        }
+
+        [TearDown]
+        public void TestTearDown()
+        {
+            Directory.Delete(_tempOutputFolder, true);
+        }
+
+        private string ExecuteXpath(string filepath, string xpath)
+        {
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load(filepath);
+            var nav = xmlDoc.CreateNavigator();
+            var expr = nav.Compile(xpath);
+            var result = (string) nav.Evaluate(expr);
+
+            return result;
+        }
+        
+        [Test]
+        public void TestMinifiedXML_even()
+        {
+            var input = new SplitXMLFileInput() { InputFilePath = _minifiedInputPath, OutputFilesDirectory = _tempOutputFolder, SplitAtElementName = "Product" };
+            var opt = new SplitXMLFileOptions() {ElementCountInEachFile = 4, OutputFileRootNodeName = "root"};
+
+            var result = XmlTasks.SplitXMLFile(input, opt, new CancellationToken());
+
+            //12 products should be split into 3 files
+            Assert.AreEqual(3, result.FilePaths.Count);
+
+            //Last file should have 4 Products
+            var productCount = ExecuteXpath(result.FilePaths.Last(), "string(count(/root/Product))");
+            Assert.AreEqual("4", productCount);
+
+            //The Last Product of first file should have id 4
+            var id = ExecuteXpath(result.FilePaths.First(), "string(/root/Product[last()]/id)");
+            Assert.AreEqual("4", id);
+
+        }
+
+        [Test]
+        public void TestMinifiedXML_uneven()
+        {
+            var input = new SplitXMLFileInput() { InputFilePath = _prettyInputPath, OutputFilesDirectory = _tempOutputFolder, SplitAtElementName = "Product" };
+            var opt = new SplitXMLFileOptions() { ElementCountInEachFile = 10, OutputFileRootNodeName = "root" };
+
+            var result = XmlTasks.SplitXMLFile(input, opt, new CancellationToken());
+
+            //12 products should be split into 2 files
+            Assert.AreEqual(2, result.FilePaths.Count);
+
+            //Last file should have 2 Products
+            var productCount = ExecuteXpath(result.FilePaths.Last(), "string(count(/root/Product))");
+            Assert.AreEqual("2", productCount);
+
+            //The Last Product of first file should have id 10
+            var id = ExecuteXpath(result.FilePaths.First(), "string(/root/Product[last()]/id)");
+            Assert.AreEqual("10", id);
+        }
+
+        [Test]
+        public void TestOutputRootElement()
+        {
+            string expectedRootElement = "TestRoot";
+
+            var input = new SplitXMLFileInput() { InputFilePath = _prettyInputPath, OutputFilesDirectory = _tempOutputFolder, SplitAtElementName = "Product" };
+            var opt = new SplitXMLFileOptions() { ElementCountInEachFile = 20, OutputFileRootNodeName = expectedRootElement };
+
+            var result = XmlTasks.SplitXMLFile(input, opt, new CancellationToken());
+
+            var rootElementName = ExecuteXpath(result.FilePaths.Last(), "local-name(/*)");
+            Assert.AreEqual(expectedRootElement, rootElementName);
+        }                
+    }
+}
---
+++ Frends.Community.Xml/SplitXMLFileTask.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Threading;
+using System.Xml;
+
+#pragma warning disable 1591
+
+namespace Frends.Community.Xml
+{
+    public static class XmlTasks
+    {
+        public static SplitXMLFileResult SplitXMLFile([PropertyTab]SplitXMLFileInput Input, [PropertyTab]SplitXMLFileOptions Options, CancellationToken cancellationToken)
+        {
+            int seqNr = 0;
+            int loopSeqNr = 0;
+            List<string> returnArray = new List<string>();
+
+            FileInfo fileInfo = new FileInfo(Input.InputFilePath);
+            DirectoryInfo dirInfo = new DirectoryInfo(Input.OutputFilesDirectory);
+
+            using (XmlReader processDoc = XmlReader.Create(Input.InputFilePath, new XmlReaderSettings() { DtdProcessing = DtdProcessing.Ignore }))
+            {
+
+                XmlDocument newDoc = InitiateNewDocument(Options.OutputFileRootNodeName);
+
+                while (processDoc.Read())
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    // Second while is needed because Read() skips elements if there is no white space between them.
+                    // This happens because ReadInnerXml() (below) moves the reader to the next element and then Read() will skip it
+                    while (processDoc.Name == Input.SplitAtElementName && processDoc.NodeType == XmlNodeType.Element && processDoc.ReadState != ReadState.EndOfFile)
+                    {
+                        cancellationToken.ThrowIfCancellationRequested();
+
+                        XmlElement outputNode = newDoc.CreateElement(processDoc.Name);
+                        outputNode.InnerXml = processDoc.ReadInnerXml();
+                        newDoc.LastChild.AppendChild(outputNode);
+
+                        // Write new file when the max element count is reached
+                        if (++loopSeqNr >= Options.ElementCountInEachFile)
+                        {
+                            string strFileName = fileInfo.Name + "." + seqNr++ + ".part";
+                            string outputFilePath = Path.Combine(dirInfo.FullName, strFileName);
+                            newDoc.Save(outputFilePath);
+                            returnArray.Add(outputFilePath);
+                            loopSeqNr = 0;
+                            newDoc = InitiateNewDocument(Options.OutputFileRootNodeName);
+                        }
+                    }
+                }
+
+                // If there are any leftover elements we create one last file
+                if (processDoc.ReadState == ReadState.EndOfFile && loopSeqNr != 0)
+                {
+                    string strFileName = fileInfo.Name + "." + seqNr + ".part";
+                    string outputFilePath = Path.Combine(dirInfo.FullName, strFileName);
+                    newDoc.Save(outputFilePath);
+                    returnArray.Add(outputFilePath);
+                }
+            }
+
+            return new SplitXMLFileResult() { FilePaths = returnArray };
+        }
+
+        private static XmlDocument InitiateNewDocument(string Rootname)
+        {
+            XmlDocument newDoc = new XmlDocument();
+            XmlDeclaration xmlDeclaration = newDoc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            XmlElement root = newDoc.DocumentElement;
+            newDoc.InsertBefore(xmlDeclaration, root);
+
+            XmlElement rootElement = newDoc.CreateElement(string.Empty, Rootname, string.Empty);
+            newDoc.AppendChild(rootElement);
+
+            return newDoc;
+        }
+    }
+}
